e6b00010c7b390f6c3dcf5caa8ca60112ba44493
Wenrui Xu <wenruix@stellar-vis1.princeton.edu>
2022-06-22 16:56:41 -0400

    update links to dataverse

--- stats_and_visualizations/download_fitted_systems.py
+++ stats_and_visualizations/download_fitted_systems.py
@@ -2,17 +2,18 @@ import requests
 from os.path import exists
 
 ids = {
-    'fit_1mm_Q1d5_age1e5.pkl': 6045345,
-    'fit_1mm_Q1_age1e5.pkl': 6045350,
-    'fit_1mm_Q2_age1e5.pkl': 6045351,
-    'fit_1mm_Q10_age1e5.pkl': 6045352,
-    'fit_1mm_Q100_age1e5.pkl': 6045353,
-    'fit_10um_Q1d5_age1e5.pkl': 6045348,
-    'fit_100um_Q1d5_age1e5.pkl': 6045349,
-    'fit_1cm_Q1d5_age1e5.pkl': 6045347,
-    'fit_1mm_Q1d5_age2e5.pkl': 6045355,
-    'fit_1mm_Q1d5_age5e4.pkl': 6045354,
-    'fit_1mm_Q1d5_age1e5_alma_only.pkl': 6045346,
+    'fit_1mm_Q1d5_age1e5.pkl': 6347016,
+    'fit_1mm_Q1_age1e5.pkl': 6347079,
+    'fit_1mm_Q2_age1e5.pkl': 6347088,
+    'fit_1mm_Q3_age1e5.pkl': 6347095,
+    'fit_1mm_Q10_age1e5.pkl': 6347007,
+    'fit_1mm_Q100_age1e5.pkl': 6346998,
+    'fit_10um_Q1d5_age1e5.pkl': 6346982,
+    'fit_100um_Q1d5_age1e5.pkl': 6346975,
+    'fit_1cm_Q1d5_age1e5.pkl': 6346991,
+    'fit_1mm_Q1d5_age2e5.pkl': 6347069,
+    'fit_1mm_Q1d5_age5e4.pkl': 6347074,
+    'fit_1mm_Q1d5_age1e5_alma_only.pkl': 6347060,
 }
 
 def download_one_file(fname, folder='../data/fitted_systems'):
